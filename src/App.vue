dfee46d721b6e1ba0a3ac9aac422fd0a52e516c5
Masterilla <masterilla@ukr.net>
2024-06-04 14:14:15 +0300

    Install Vuetify and base setting

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,15 @@
 </script>
 
 <template>
-  <div>content</div>
-  <button>Press</button>
+  <v-app class="rounded rounded-md">
+    <v-app-bar title="Application bar"></v-app-bar>
+    <v-main>
+      <div>Content</div>
+      <v-btn>
+        Button
+      </v-btn>
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import vuetify from './plugins/vuetify.js'
 
 const app = createApp(App)
 
+app.use(vuetify)
 app.use(createPinia())
 app.use(router)
 
