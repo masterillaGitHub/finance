816df99d870d2b08f058227a7c2e2222771dadd0
Masterilla <masterilla@ukr.net>
2024-06-04 13:40:55 +0300

    Setting server with custom host, port and https

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import mkcert from 'vite-plugin-mkcert'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: 'finance.local',
+    port: '5200',
+    strictPort: true,
+    https: true,
+  },
   plugins: [
     vue(),
     VueDevTools(),
+    mkcert()
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    },
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json', '.vue']
   }
 })
