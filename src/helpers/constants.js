125ed4740c45a90593d207b5be62ed1321e63f31
Masterilla <masterilla@ukr.net>
2024-06-07 13:29:33 +0300

    Added constants.js

---
+++ src/helpers/constants.js
@@ -0,0 +1,10 @@
+export const BASE_URL = import.meta.env.VITE_APP_API_URL
+export const STORAGE_URL = import.meta.env.VITE_APP_API_URL + '/storage'
+
+export const MODEL_UPDATE_ALL = 1
+export const MODEL_UPDATE_ENTITY = 2
+export const MODEL_UPDATE_FIELD = 3
+
+export const LAYOUT_ACCOUNT = 'Account'
+export const LAYOUT_MAIN = 'Main'
+export const LAYOUT_AUTH = 'Auth'
