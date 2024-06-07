e7ca34bca789877205ff35f86312800103de3cbe
Masterilla <masterilla@ukr.net>
2024-06-07 16:06:26 +0300

    Axios: Correct import router into axios handler

--- src/plugins/axios/response_error.js
+++ src/plugins/axios/response_error.js
@@ -1,9 +1,8 @@
 import {useAuthStore} from "@/stores/auth.store.js";
-import {useRouter} from "vue-router";
+import router from '@/router'
 
 export default function ($axiosError) {
     const authStore = useAuthStore()
-    const router = useRouter()
 
     if ($axiosError.response.status === 401 || $axiosError.response.status === 419) {
         authStore.resetUser()
