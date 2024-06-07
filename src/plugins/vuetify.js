c11b202963370d2797737c51012bbd70860dbbd1
Masterilla <masterilla@ukr.net>
2024-06-07 13:26:21 +0300

    Plugin,Vuetify: Setting library of icons

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,9 +4,14 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import '@mdi/font/css/materialdesignicons.css'
+
 const vuetify = createVuetify({
     components,
     directives,
+    icons: {
+        defaultSet: 'mdi',
+    },
 })
 
 export default vuetify
