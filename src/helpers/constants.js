6aac4b6e95d201a9c4467d8e9e64942fd2259b07
Masterilla <masterilla@ukr.net>
2024-06-09 12:22:56 +0300

    constants.js: added some constants for ACCOUNT

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -6,5 +6,11 @@ export const MODEL_UPDATE_ENTITY = 2
 export const MODEL_UPDATE_FIELD = 3
 
 export const LAYOUT_ACCOUNT = 'Account'
+export const LAYOUT_ACCOUNT_FULLSCREEN = 'AccountFullscreen'
 export const LAYOUT_MAIN = 'Main'
 export const LAYOUT_AUTH = 'Auth'
+
+export const ACCOUNT_TYPE_BANK = 'bank'
+export const ACCOUNT_TYPE_CASH = 'cash'
+export const ACCOUNT_TYPE_OTHER = 'other'
+export const ACCOUNT_TYPE_DEBT = 'debt'
