686e2bd1eb51feae9f74ae58f8779f2e88a14b50
Masterilla <masterilla@ukr.net>
2024-06-09 12:30:10 +0300

    Create component 'TheDialog' with check on 'mobile'

---
+++ src/components/TheDialog.vue
@@ -0,0 +1,22 @@
+<script setup>
+
+import {useDisplay} from "vuetify";
+
+const { mobile } = useDisplay()
+const dialog = defineModel()
+
+</script>
+
+<template>
+  <v-dialog
+      max-width="600"
+      :fullscreen="mobile"
+      v-model="dialog"
+  >
+    <slot></slot>
+  </v-dialog>
+</template>
+
+<style scoped>
+
+</style>
