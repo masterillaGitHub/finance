ab367df9933389fd9f9145d9f0b1c4e34787c35c
Masterilla <masterilla@ukr.net>
2024-06-09 12:24:41 +0300

    plugins,axios: Change in 'response_error.js'

--- src/plugins/axios/response_error.js
+++ src/plugins/axios/response_error.js
@@ -6,6 +6,7 @@ export default function ($axiosError) {
 
     if ($axiosError.response.status === 401 || $axiosError.response.status === 419) {
         authStore.resetUser()
+        router.push({name: 'login'})
     }
 
     return Promise.reject($axiosError)
