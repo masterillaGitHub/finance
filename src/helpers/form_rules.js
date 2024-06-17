001bd5914a050affcfc01b063932122852ff0dfb
Masterilla <masterilla@ukr.net>
2024-06-17 14:13:00 +0300

    helpers: Update 'form_rules' added method 'requiredZeroPossible'

--- src/helpers/form_rules.js
+++ src/helpers/form_rules.js
@@ -1,13 +1,25 @@
-import {isNumber} from "@/helpers/validators.js";
+import {isNotEmpty, isNumber, isNumberInString} from "@/helpers/validators/index.js";
 
-export function requiredRule(value) {
-    if (value) return true
+export function required(value) {
+    if (isNotEmpty(value)) return true
 
     return 'Поле не може бути пустим.'
 }
 
-export function numberRule(value) {
+export function requiredZeroPossible(value) {
+    if (isNotEmpty(value) || value === 0) return true
+
+    return 'Поле не може бути пустим.'
+}
+
+export function integer(value) {
     if (isNumber(value)) return true
+
+    return 'Значення повинно бути цілим числом.'
+}
+
+export function number(value) {
+    if (isNumberInString(value)) return true
 
     return 'Значення повинно бути числом.'
 }
