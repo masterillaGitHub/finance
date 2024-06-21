0058689a0d01d66aa65e1f22be21140636c19bff
Masterilla <masterilla@ukr.net>
2024-06-21 15:48:50 +0300

    ResourceModels: Currency.js. added getter 'icon'

--- src/models_resources/models/Currency.js
+++ src/models_resources/models/Currency.js
@@ -1,8 +1,13 @@
 import Model from "@/models_resources/models/Model";
+import {CURRENCY_ICONS} from "@/helpers/constants.js";
 
 
 export default class Currency extends Model
 {
     _resourceName = 'currencies'
 
+
+    get icon() {
+        return CURRENCY_ICONS.find(icon => icon.id === this.id)?.icon ?? null
+    }
 }
