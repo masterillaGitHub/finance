679b6b36251edce04dba4e3d2e87b360e8ff74c4
Masterilla <masterilla@ukr.net>
2024-06-21 15:58:11 +0300

    Vite production build

---
+++ dist/assets/Budgets-CE5wEouX.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as t}from"./index-B9rgIAwP.js";const n={};function o(r,s){return c(),t("div",null,"Budgets")}const a=e(n,[["render",o]]);export{a as default};
---
+++ dist/assets/show.store-BLD24Lg_.js
@@ -0,0 +1,1 @@
+import{E as t}from"./index-B9rgIAwP.js";const o=t("accounts/show",{state:()=>({editDialog:!1}),getters:{},actions:{}});export{o as u};
