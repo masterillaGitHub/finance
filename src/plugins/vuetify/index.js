e1b5e076614ed60621dc2c15f200628fcd84891c
Masterilla <masterilla@ukr.net>
2024-07-05 19:06:50 +0300

    vuetify: Added 'lib' component

--- src/plugins/vuetify/index.js
+++ src/plugins/vuetify/index.js
@@ -3,12 +3,15 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { VPullToRefresh } from 'vuetify/labs/VPullToRefresh'
 import ua from './locales/ua'
 
 import '@mdi/font/css/materialdesignicons.css'
 
+const com = Object.assign({...components}, {VPullToRefresh})
+
 const vuetify = createVuetify({
-    components,
+    components: com,
     directives,
     icons: {
         defaultSet: 'mdi',
