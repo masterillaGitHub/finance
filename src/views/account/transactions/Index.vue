332c7ce43e71943667851b0768c4ab5d41634f2c
Masterilla <masterilla@ukr.net>
2024-07-05 11:20:54 +0300

    Transaction,Index: Fixed 'jumping' button for adding transaction

--- src/views/account/transactions/Index.vue
+++ src/views/account/transactions/Index.vue
@@ -1,20 +1,31 @@
 <script setup>
 
 import TransactionGroup from "@/views/account/transactions/components/index/TransactionGroup.vue";
-import {computed, onMounted} from "vue";
+import {computed, onMounted, ref} from "vue";
 import Transaction from "@/models_resources/models/Transaction.js";
 import {isUndefined} from "@/helpers/validators/index.js";
 import {useIndexStore} from "@/stores/transactions/index.store.js";
-import TheBottomLoading from "@/views/account/transactions/components/index/TheBottomLoading.vue";
+import TheBottomLazyLoading from "@/views/account/transactions/components/index/TheBottomLazyLoading.vue";
+import {useRouter} from "vue-router";
 
+const router = useRouter()
 const indexStore = useIndexStore()
 const transactionsGroups = computed(() => toGroupTransactions(Transaction.findLoaded()))
+const isAddButton = ref(false)
 
 onMounted(initComponent)
 
 function initComponent() {
+  setTimeout(() => {
+    isAddButton.value = true
+  },1000)
+
   indexStore.loadTransactions()
 }
+
+router.beforeEach((to, from) => {
+  isAddButton.value = false
+})
 
 function toGroupTransactions(transactions) {
   return Object.values(transactions.reduce(createGroup, {}))
@@ -42,6 +53,7 @@ function createGroup(acc, transaction) {
 </script>
 <template>
 <div
+    class="pa-2"
 >
   <v-card
     :loading="indexStore.transactionsLoading"
@@ -52,18 +64,19 @@ function createGroup(acc, transaction) {
           v-for="group in transactionsGroups" :key="group.timestamp"
           :group="group"
       />
-      <TheBottomLoading/>
-      <v-fab
-          app
-          icon="mdi-plus"
-          class="mb-14"
-          :to="{name: 'transactions.create'}"
-      />
+      <TheBottomLazyLoading/>
     </v-card-text>
   </v-card>
+
+    <v-fab
+        :active="isAddButton"
+        icon="mdi-plus"
+        :to="{name: 'transactions.create'}"
+        app
+        class="mb-14"
+    />
 </div>
 </template>
 
 <style scoped>
-
 </style>
