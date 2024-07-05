562267381c4e8b9b895026fac7e8a6b816ecc78b
Masterilla <masterilla@ukr.net>
2024-07-05 19:10:31 +0300

    Vite production build

--- dist/assets/show.store-B-m4Unvd.js
+++
@@ -1,1 +0,0 @@
-import{G as t}from"./index-Dq0Qrvv2.js";const o=t("accounts/show",{state:()=>({editDialog:!1}),getters:{},actions:{}});export{o as u};
---
+++ dist/assets/show.store-BuE497LD.js
@@ -0,0 +1,1 @@
+import{G as t}from"./index-Be_DmAD-.js";const o=t("accounts/show",{state:()=>({editDialog:!1}),getters:{},actions:{}});export{o as u};
