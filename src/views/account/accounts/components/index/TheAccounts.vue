da9ee5152a1d183ce1df2d8d492ce08743b6c693
Masterilla <masterilla@ukr.net>
2024-07-09 14:49:56 +0300

    Accounts: Change loading style

--- src/views/account/accounts/components/index/TheAccounts.vue
+++ src/views/account/accounts/components/index/TheAccounts.vue
@@ -6,6 +6,7 @@ import AccountCategory from "@/models_resources/models/AccountCategory.js";
 import {useIndexStore} from "@/stores/accounts/index.store.js";
 import Account from "@/models_resources/models/Account.js";
 import AccountSum from "@/models_resources/models/AccountSum.js";
+import {isEmpty} from "@/helpers/validators/index.js";
 
 const indexStore = useIndexStore()
 const categoriesIds = ref([])
@@ -13,9 +14,6 @@ const categories = computed(() => AccountCategory.findIn(indexStore.categoriesId
 const accountsLoading = ref(false)
 
 onMounted(async () => {
-  AccountCategory.reset()
-  Account.reset()
-  AccountSum.reset()
   await load()
 })
 
@@ -24,6 +22,11 @@ async function load() {
 
   try {
     const response = await AccountCategory.loadMainPage()
+
+    if (isEmpty(response.data.data)) {
+      resetAccountCategories()
+    }
+
     // TODO: потрібно було зняти реактивність тому що дублювались дані із викликом нового об'єкту з іншого компоненту
     indexStore.categoriesIds = [...response.data.data]
   }
@@ -32,29 +35,41 @@ async function load() {
   }
 
 }
+
+function resetAccountCategories() {
+  AccountCategory.reset()
+  Account.reset()
+  AccountSum.reset()
+}
 </script>
 
 <template>
   <div>
     <v-btn text="load" @click="load" class="mb-4"/>
-
-    <v-progress-linear
-        v-if="accountsLoading"
-        indeterminate
-    />
-
-    <div v-if="categories.length">
-      <v-expansion-panels
-          multiple
-          v-model="indexStore.accountsPanelModel"
-      >
-        <AccountGroupItem
-            v-for="element in categories"
-            :key="element.id"
-            :category="element"
+    <div style="height: 4px">
+      <v-fade-transition>
+        <v-progress-linear
+            v-show="accountsLoading"
+            indeterminate
         />
-      </v-expansion-panels>
+      </v-fade-transition>
     </div>
+
+    <v-fade-transition mode="out-in">
+      <div v-if="categories.length">
+        <v-expansion-panels
+            multiple
+            v-model="indexStore.accountsPanelModel"
+        >
+          <AccountGroupItem
+              v-for="element in categories"
+              :key="element.id"
+              :category="element"
+          />
+        </v-expansion-panels>
+      </div>
+    </v-fade-transition>
+
   </div>
 
 </template>
