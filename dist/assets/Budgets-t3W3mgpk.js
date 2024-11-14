0b95f4789cf9d3c4902e0a5d85221817dd8eb51f
Masterilla <masterilla@ukr.net>
2024-11-14 17:24:46 +0200

    Vite production build

--- dist/assets/Budgets-t3W3mgpk.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as s}from"./index-CMFz-0SU.js";const t={},o={class:"pa-2"};function n(r,_){return c(),s("div",o,"Budgets")}const d=e(t,[["render",n]]);export{d as default};
--- dist/assets/Dashboard-Btn5BwNI.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as t,o as n,c as a,j as c}from"./index-CMFz-0SU.js";const r={},s={class:"pa-2"};function _(l,i){const e=t("v-alert");return n(),a("div",s,[c(e,{icon:"$vuetify",title:"Welcome to the world of finance",type:"info",variant:"tonal"})])}const d=o(r,[["render",_]]);export{d as default};
