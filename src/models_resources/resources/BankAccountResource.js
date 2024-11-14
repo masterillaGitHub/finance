a9596bdf7b63a23d0405ad439ba191c74b3b7f1f
Masterilla <masterilla@ukr.net>
2024-11-14 17:07:27 +0200

    feat: add button for sync bank transactions

--- src/models_resources/resources/BankAccountResource.js
+++ src/models_resources/resources/BankAccountResource.js
@@ -5,6 +5,14 @@ export default class BankAccountResource extends ApiResource
     getResource() {
         return 'bank-accounts';
     }
+
+    static updateTransactions(bankId) {
+        return (new this()).updateTransactions(bankId)
+    }
+
+    async updateTransactions(bankId) {
+        return await this.post(this.getPath() + '/update-transactions', {bankId})
+    }
 }
 
 
--- src/views/account/accounts/Index.vue
+++ src/views/account/accounts/Index.vue
@@ -3,6 +3,21 @@
 import TheWealthChart from "@/views/account/accounts/components/index/TheWealthChart.vue";
 import TheAccounts from "@/views/account/accounts/components/index/TheAccounts.vue";
 import TheAddAccount from "@/views/account/accounts/components/create/TheAddAccount.vue";
+import BankAccountResource from "@/models_resources/resources/BankAccountResource.js";
+import {BANK_ID_MONO} from "@/helpers/constants.js";
+import {ref} from "vue";
+
+const accountLoading = ref(false)
+const updateAccounts = async () => {
+  accountLoading.value = true
+  try {
+    await BankAccountResource.updateTransactions(BANK_ID_MONO)
+  }
+  finally {
+    accountLoading.value = false
+  }
+}
+
 </script>
 
 <template>
@@ -10,6 +25,13 @@ import TheAddAccount from "@/views/account/accounts/components/create/TheAddAcco
     <TheWealthChart class="mb-4"/>
     <TheAccounts class="mb-5"/>
     <TheAddAccount/>
+    <v-btn
+        variant="outlined"
+        :loading="accountLoading"
+        @click="updateAccounts"
+    >
+      <v-icon icon="mdi-sync"/>
+    </v-btn>
   </div>
 </template>
 
