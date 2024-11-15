8fee258f1f07bd9747b93532a68b5b2bff4eb769
Masterilla <masterilla@ukr.net>
2024-11-15 16:54:58 +0200

    Vite production build

--- dist/assets/BankAccountResource-CEDYndJS.js
+++
@@ -1,1 +0,0 @@
-import{a7 as a}from"./index-BbjtBCCf.js";class n extends a{getResource(){return"bank-accounts"}static updateTransactions(t){return new this().updateTransactions(t)}async updateTransactions(t){return await this.post(this.getPath()+"/update-transactions",{bankId:t})}}export{n as B};
