76374be0bbe5f3e5b7c6e08613f6d2e6fa332c72
Masterilla <masterilla@ukr.net>
2024-12-20 14:45:22 +0200

    feat: add icon to Dashboard page

--- src/views/account/Dashboard.vue
+++ src/views/account/Dashboard.vue
@@ -1,15 +1,21 @@
 <script setup>
 
+const imgUrl = new URL('/assets/images/icons/favicon.svg', import.meta.url).href
 </script>
 
 <template>
 <div class="pa-2">
   <v-alert
-      icon="$vuetify"
+      :icon="imgUrl"
       type="info"
       variant="tonal"
   >
-    Welcome to <b>Denarius</b> - the world of finance
+    <template #prepend>
+      <v-avatar :image="imgUrl"/>
+    </template>
+    <template #text>
+      Welcome to <b>Denarius</b> - the world of finance
+    </template>
   </v-alert>
 
 </div>
